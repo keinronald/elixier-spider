440b606e774b2f5b31b287c6874926c8f372b7d1
Ronald Keinberger <r.keinberger@live.de>
2020-01-15 19:50:39 +0100

    refactor redirect status codes

--- lib/links/fetcher.ex
+++ lib/links/fetcher.ex
@@ -1,18 +1,11 @@
 defmodule Links.Fetcher do
 
     def fetch(url) do
-        case HTTPoison.get!(url) do
+        case HTTPoison.get!(url, [], follow_redirect: true) do
             %HTTPoison.Response{status_code: 200, body: html} ->
-                html
-            %HTTPoison.Response{status_code: 301, body: html} ->
-                html
-            %HTTPoison.Response{status_code: 302, body: html} ->
                 html
             %HTTPoison.Error{reason: reason} ->
                 IO.puts(reason)
-                nil
-            _ ->
-                IO.puts("unwanted status code.")
                 nil
         end
     end
